b3344db1d99890be2859312fc9cd62ca79c66fcb
alex.vysocki <sasha.vysocki@gmail.com>
2021-04-27 21:40:02 +0300

    Complete

--- components/PageTransaction.js
+++ components/PageTransaction.js
@@ -28,7 +28,7 @@ class PageTransaction {
             <header class="header">
                 <div class="container">
 
-                    <button class="page_transaction_back_button" onClick="/FinanceApp/#cards"><</button>
+                    <button class="page_transaction_back_button" onClick="location.href='/FinanceApp/#cards'"><</button>
                     <h1>Transactions</h1>
 
                 </div>
